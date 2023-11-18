121e75ac71073c27d7e4323fe9215056c8d68a30
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2023-11-19 01:49:11 +0600

    add firebase authentication fix bug

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,11 +4,13 @@ import { toast } from "react-hot-toast";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider";
 import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
+import { useForm } from "react-hook-form";
 
 
 const Login = () => {
-   const [email, setEmail] = useState('');
-   const [password, setPassword] = useState('');
+   const { register, handleSubmit, formState: { errors }, } = useForm();
+   // const [email, setEmail] = useState('');
+   // const [password, setPassword] = useState('');
    const navigate = useNavigate();
    const { signIn } = useContext(AuthContext);
 
@@ -19,18 +21,18 @@ const Login = () => {
       loadCaptchaEnginge(6);
    }, [])
 
-   const handleValidateCaptcha = () => {
-      const user_captcha_value = captchaRef.current.value;
+   const handleValidateCaptcha = (e) => {
+      // const user_captcha_value = captchaRef.current.value;
+      const user_captcha_value = e.target.value;
       if (validateCaptcha(user_captcha_value)) {
          setDisabled(false);
       }
    }
-   const handleLogin = async (e) => {
-      e.preventDefault();
-      // console.log(email,password);
+   const onSubmit = async (data) => {
+      console.log(data);
       const toastId = toast.loading('Logging In....');
       try {
-         await signIn(email, password)
+         await signIn(data.email, data.password)
          toast.success('LogIn Successfully!!', { id: toastId })
          if (location.state && location.state.from) {
             navigate(location.state.from.pathname)
@@ -55,36 +57,40 @@ const Login = () => {
                      LogIn Now!!
                   </h3>
                </div>
-               <form onSubmit={handleLogin}>
-                  <div className="flex flex-col gap-4 p-6">
+               <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="flex flex-col gap-8 p-6">
                      <div className="relative h-11 w-full min-w-[200px]">
                         <input type="email"
-                           onBlur={(e) => setEmail(e.target.value)}
+                           {...register("email", { required: true })}
+                           // onBlur={(e) => setEmail(e.target.value)}
                            className="w-full h-full px-3 py-3  text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                           required />
+                        />
+                        {errors.email && <span className="text-red-600 text-xs font-medium">Email is required</span>}
                         <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                            Email
                         </label>
                      </div>
                      <div className="relative h-11 w-full min-w-[200px]">
                         <input type="password"
-                           onBlur={(e) => setPassword(e.target.value)}
+                           {...register("password", { required: true })}
+                           // onBlur={(e) => setPassword(e.target.value)}
                            className="w-full h-full px-3 py-3  text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200 border-t-transparent text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
-                           required />
+                        />
+                        {errors.password && <span className="text-red-600 text-xs font-medium">Password is required</span>}
                         <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
                            Password
                         </label>
                      </div>
                      <LoadCanvasTemplate />
                      <div className=" h-11 w-full flex items-center gap-4 min-w-[200px]">
-                        <input type="text" ref={captchaRef}
+                        <input type="text"
+                           onBlur={handleValidateCaptcha}
+                           ref={captchaRef}
                            placeholder="Type the captcha above..."
                            className="w-full h-full px-3 py-3  text-sm font-normal transition-all bg-transparent border rounded-md peer border-blue-gray-200  text-blue-gray-700 outline outline-0 placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
                            required />
-                        <button onBlur={handleValidateCaptcha} className="btn btn-outline btn-xs btn-success">validate</button>
-
+                        {/* <button onBlur={handleValidateCaptcha} className="btn btn-outline btn-xs btn-success">validate</button> */}
                      </div>
-
                   </div>
 
                   <div className="p-6 pt-0">
